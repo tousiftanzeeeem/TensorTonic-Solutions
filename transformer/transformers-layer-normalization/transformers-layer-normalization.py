6c586e3569501bb334bd80fc8bf70b172494575c
Tanzim Tousif <142239709+tousiftanzeeeem@users.noreply.github.com>
2026-08-27 01:04:29 +0600

    Solve transformers-layer-normalization

---
+++ transformer/transformers-layer-normalization/transformers-layer-normalization.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def layer_norm(x: np.ndarray, gamma: np.ndarray, beta: np.ndarray, eps: float = 1e-6) -> np.ndarray:
+    """
+    Returns: Normalized array of same shape as x
+    """
+    mean = x.mean(axis=-1, keepdims=True)
+    var = x.var(axis=-1,keepdims=True)
+    return gamma * ((x-mean)/np.sqrt(var + eps)) + beta
