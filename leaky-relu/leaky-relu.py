39fa11b560beaf94a78aca7c769b6a27b1214689
Tanzim Tousif <142239709+tousiftanzeeeem@users.noreply.github.com>
2026-08-27 01:15:42 +0600

    Solve leaky-relu

---
+++ leaky-relu/leaky-relu.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def leaky_relu(x: list | float, alpha: float = 0.01) -> np.ndarray:
+    """
+    Returns elementwise Leaky ReLU values as a NumPy array matching the input shape.
+    """
+    x = np.asarray(x)
+    return np.where(x < 0,alpha * x,x)
